c08e751938d746bb953623bd9129300b645e0c97
Jeremias Quevedo <jeremias22xt@gmail.com>
2022-10-05 21:47:45 -0300

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -49,7 +49,7 @@ export async function getStaticPaths() {
 
     return {
         paths: meetups.map(meetup => ({params: {meetupId: meetup._id.toString()}})),
-        fallback: false
+        fallback: "blocking"
     };
 }
 
